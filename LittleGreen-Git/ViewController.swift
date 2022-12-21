6b62a3cb0e8ed3d69eaa0bfb2b0f42c4fa9d351f
“王斯嘉” <sjld116@126.com>
2022-12-21 13:25:36 +0800

    Initial Commit

---
+++ LittleGreen-Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittleGreen-Git
+//
+//  Created by 王斯嘉 on 2022/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
